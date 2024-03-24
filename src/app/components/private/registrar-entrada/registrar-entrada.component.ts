e1e909ab87d3ca9281350afa3ddf256261df6dfa
FASDA0311 <fserrepe@unprg.edu.pe>
2024-03-23 22:22:59 -0500

    RegistrarEntrada Creacion

---
+++ src/app/components/private/registrar-entrada/registrar-entrada.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrar-entrada',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrar-entrada.component.html',
+  styleUrl: './registrar-entrada.component.scss'
+})
+export class RegistrarEntradaComponent {
+
+}
